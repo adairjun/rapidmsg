745ca199758746c5a31f707b32cedff709e8a9e0
Xiong Jun <adairjun@gmail.com>
2016-01-13 18:38:13 +0800

    MOD: modify rmessage_util

--- include/rapidmsg/rmessage_util.h
+++ include/rapidmsg/rmessage_util.h
@@ -1,13 +1,19 @@
 #ifndef _RMESSAGE_UTIL_H_
 #define _RMESSAGE_UTIL_H_
 
+#include "rapidmsg.pb.h"
 #include <string>
 
 using std::string;
 
-// 利用protobuf自动反射消息类型来创建一个message对象
-// 需要手动delete
-template<typename T>
-T* NewEmptyMessageWithName();
+/*
+ *  利用protobuf自动反射消息类型来创建一个message对象
+ *  需要手动delete
+ *  返回的google::protobuf::Message* 可以利用dynamic_cast<>来转换成对应的message的类型
+ */
+google::protobuf::Message* CreateMessage(const string& type_name);
 
-#endif
+// 根据Request来创建一个Response，如果pResponse为NULL，需要调用FreeMssage释放返回的消息句柄
+void MakeResponse(const ::rapidmsg::RMessage *pReq, int messageType_, ::rapidmsg::RMessage *pRes);
+
+#endif /* _RMESSAGE_UTIL_H_ */
--- util/rmessage_util.cpp
+++ util/rmessage_util.cpp
@@ -2,16 +2,43 @@
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/dynamic_message.h>
 
-template<typename T>
-T* NewEmptyMessageWithName() {
+/*
+ * protobuf反射
+ */
+google::protobuf::Message* CreateMessage(const string& type_name) {
 
-  string type_name = T::descriptor()->full_name();
+  google::protobuf::Message* message = NULL;
   const google::protobuf::Descriptor* descriptor =
 	    google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(type_name);
 
-  const google::protobuf::Message* prototype =
-      google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
-  T* new_obj = dynamic_cast<T*>(prototype->New());
-  return new_obj;
+  if (descriptor) {
+    const google::protobuf::Message* prototype =
+	      google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+	if (prototype) {
+	  message = prototype->New();
+	}
+  }
+  return message;
 }
 
+
+/**
+ * 根据请求包构造应答包的消息头
+ **/
+void MakeResponse(const ::rapidmsg::RMessage *pReq, int messageType_, ::rapidmsg::RMessage *pRes) {
+
+  if (pReq == NULL || !pReq->has_head()) {
+    LOG(ERROR) << " pReq is null pointer or don't have head\n";
+  }
+  if (pRes == NULL) {
+    LOG(ERROR) << "pRes is null pointer\n";
+  }
+
+  if (pReq->head.has_session_no()) { pRes->mutable_head()->set_session_no(pReq->head.session_no()); }
+
+  pRes->mutable_head()->set_message_type(messageType_);
+
+  if (pReq->head.has_client_ip()) { pRes->mutable_head()->set_client_ip(pReq->head.client_ip()); }
+  if (pReq->head.has_target_ip()) { pRes->mutable_head()->set_target_ip(pReq->head.target_ip()); }
+  if (pReq->head.has_target_port()) { pRes->mutable_head()->set_target_port(pReq->head.target_port()); }
+}
