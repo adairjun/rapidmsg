f7420ba76c05981293937221eda51272097b7207
Xiong Jun <adairjun@gmail.com>
2016-01-13 23:41:20 +0800

    MOD:modify rmessage_util

--- include/rapidmsg/rmessage_util.h
+++ include/rapidmsg/rmessage_util.h
@@ -3,17 +3,31 @@
 
 #include "rapidmsg.pb.h"
 #include <string>
+#include <boost/shared_ptr.hpp>
 
 using std::string;
+
+boost::shared_ptr<::rapidmsg::RMessage> NewMessage(const string& session_no,
+		int message_type);
 
 /*
  *  利用protobuf自动反射消息类型来创建一个message对象
  *  需要手动delete
  *  返回的google::protobuf::Message* 可以利用dynamic_cast<>来转换成对应的message的类型
  */
-google::protobuf::Message* CreateMessage(const string& type_name);
+boost::shared_ptr<::google::protobuf::Message> CreateMessage(const string& type_name);
 
-// 根据Request来创建一个Response，如果pResponse为NULL，需要调用FreeMssage释放返回的消息句柄
-void MakeResponse(const ::rapidmsg::RMessage *pReq, int messageType_, ::rapidmsg::RMessage *pRes);
+/* 根据Request来创建一个Response，如果pResponse为NULL，需要调用FreeMssage释放返回的消息句柄
+ */
+void MakeResponse(const ::rapidmsg::RMessage& pReq, int messageType_, ::rapidmsg::RMessage& pRes);
+
+/*
+ * 获取得到debug信息
+ */
+string DebugString(const ::rapidmsg::RMessage& pReq);
+
+/*
+ * 使用protobuf的GetReflection动态设置字段的值
+ */
 
 #endif /* _RMESSAGE_UTIL_H_ */
--- util/rmessage_util.cpp
+++ util/rmessage_util.cpp
@@ -2,12 +2,20 @@
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/dynamic_message.h>
 
+boost::shared_ptr<::rapidmsg::RMessage> NewMessage(const string& session_no,
+		int message_type) {
+  boost::shared_ptr<::rapidmsg::RMessage> pMessage(new ::rapidmsg::RMessage);
+  pMessage->mutable_head()->set_session_no(session_no);
+  pMessage->mutable_head()->set_message_type(message_type);
+
+  return pMessage;
+}
+
 /*
  * protobuf反射
  */
-google::protobuf::Message* CreateMessage(const string& type_name) {
+boost::shared_ptr<::google::protobuf::Message> CreateMessage(const string& type_name) {
 
-  google::protobuf::Message* message = NULL;
   const google::protobuf::Descriptor* descriptor =
 	    google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(type_name);
 
@@ -15,30 +23,36 @@ google::protobuf::Message* CreateMessage(const string& type_name) {
     const google::protobuf::Message* prototype =
 	      google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
 	if (prototype) {
-	  message = prototype->New();
+		boost::shared_ptr<::google::protobuf::Message> message(prototype->New());
+	    return message;
 	}
   }
-  return message;
+  throw std::runtime_error("Can't CreateMessage");
 }
 
 
 /**
  * 根据请求包构造应答包的消息头
  **/
-void MakeResponse(const ::rapidmsg::RMessage *pReq, int messageType_, ::rapidmsg::RMessage *pRes) {
+void MakeResponse(const ::rapidmsg::RMessage& pReq, int messageType_, ::rapidmsg::RMessage& pRes) {
 
-  if (pReq == NULL || !pReq->has_head()) {
-    LOG(ERROR) << " pReq is null pointer or don't have head\n";
-  }
-  if (pRes == NULL) {
-    LOG(ERROR) << "pRes is null pointer\n";
+  if (!pReq.has_head()) {
+	return;
   }
 
-  if (pReq->head.has_session_no()) { pRes->mutable_head()->set_session_no(pReq->head.session_no()); }
+  // request是什么session_no，就响应什么session_no。因为一个回话session势必有request和response
+  if (pReq.head().has_session_no()) { pRes.mutable_head()->set_session_no(pReq.head().session_no()); }
 
-  pRes->mutable_head()->set_message_type(messageType_);
+  pRes.mutable_head()->set_message_type(messageType_);
 
-  if (pReq->head.has_client_ip()) { pRes->mutable_head()->set_client_ip(pReq->head.client_ip()); }
-  if (pReq->head.has_target_ip()) { pRes->mutable_head()->set_target_ip(pReq->head.target_ip()); }
-  if (pReq->head.has_target_port()) { pRes->mutable_head()->set_target_port(pReq->head.target_port()); }
+  // 反转client以及target，request的client是response的target
+  if (pReq.head().has_client_ip()) { pRes.mutable_head()->set_target_ip(pReq.head().client_ip()); }
+  if (pReq.head().has_client_port()) { pRes.mutable_head()->set_target_port(pReq.head().client_port()); }
+
+  if (pReq.head().has_target_ip()) { pRes.mutable_head()->set_client_ip(pReq.head().target_ip()); }
+  if (pReq.head().has_target_port()) { pRes.mutable_head()->set_client_port(pReq.head().target_port()); }
+}
+
+string DebugString(const ::rapidmsg::RMessage& pReq) {
+  return pReq.DebugString();
 }
