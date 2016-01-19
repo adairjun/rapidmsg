fdbd56f8955d12749efa130fd221287e74839926
Xiong Jun <adairjun@gmail.com>
2016-01-19 15:58:38 +0800

    ADD: add GetFieldByPointer

--- include/rapidmsg/rmessage_util.h
+++ include/rapidmsg/rmessage_util.h
@@ -17,6 +17,11 @@ boost::shared_ptr<::rapidmsg::RMessage> NewMessage(const string& session_no,
  */
 boost::shared_ptr<::google::protobuf::Message> CreateMessage(const string& type_name);
 
+/*
+ * 从已经序列化的消息当中反序列化
+ */
+boost::shared_ptr<::google::protobuf::Message> ParseFromStr(const string& messageType, const string& message);
+
 /* 根据Request来创建一个Response，如果pResponse为NULL，需要调用FreeMssage释放返回的消息句柄
  */
 void MakeResponse(const ::rapidmsg::RMessage& pReq, int messageType_, ::rapidmsg::RMessage& pRes);
@@ -26,19 +31,17 @@ void MakeResponse(const ::rapidmsg::RMessage& pReq, int messageType_, ::rapidmsg
  */
 string DebugString(const ::rapidmsg::RMessage& pReq);
 
-/*
- * 使用protobuf的GetReflection动态设置字段的值
- */
-/*
- * 从已经序列化的消息当中取出一个在proto文件当中定义的字段
- * 比如
- * message test{
- *   require string ip = 1;
- *   require uint32 port = 2;
- * }
- * 对于test这个message，这个函数能将序列化之后的string当中获取字段ip或者port的值
- */
+int32_t GetInt32FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+uint32_t GetUInt32FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
 
-string GetStringFieldByMessage(const string& messageType, const string& message, const string& fieldName);
+int64_t GetInt64FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+uint64_t GetUInt64FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+
+float GetFloatFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+double GetDoubleFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+
+bool GetBoolFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+string GetStringFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index);
+
 
 #endif /* _RMESSAGE_UTIL_H_ */
--- util/rmessage_util.cpp
+++ util/rmessage_util.cpp
@@ -30,6 +30,13 @@ boost::shared_ptr<::google::protobuf::Message> CreateMessage(const string& type_
   throw std::runtime_error("Can't CreateMessage");
 }
 
+boost::shared_ptr<::google::protobuf::Message> ParseFromStr(const string& messageType, const string& message) {
+  const ::google::protobuf::Descriptor* descriptor = ::google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(messageType);
+  const ::google::protobuf::Message* prototype = ::google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+  boost::shared_ptr<::google::protobuf::Message> new_obj(prototype->New());
+  new_obj->ParseFromString(message);
+  return new_obj;
+}
 
 /**
  * 根据请求包构造应答包的消息头
@@ -60,36 +67,122 @@ string DebugString(const ::rapidmsg::RMessage& pReq) {
   return pReq.DebugString();
 }
 
-string GetStringFieldByMessage(const string& messageType, const string& message, const string& fieldName) {
-
-  const ::google::protobuf::Descriptor* descriptor = ::google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(messageType);
-  const ::google::protobuf::Message* prototype = ::google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
-  boost::shared_ptr<google::protobuf::Message> new_obj(prototype->New());
-  new_obj->ParseFromString(message);
+int32_t GetInt32FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
 
   const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
-  const ::google::protobuf::FieldDescriptor* rmes_field = descriptor->FindFieldByName(fieldName);
-  return reflection->GetString(*new_obj, rmes_field);
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_INT32) {
+    if (rmes_field->is_repeated()) {
+	  //判断是不是repeated字段
+	  return reflection->GetRepeatedInt32(*new_obj, rmes_field, index);
+	} else {
+	  return reflection->GetInt32(*new_obj, rmes_field);
+	}
+  }
+  return -1;
+}
 
-  /*if (typeid(T) == typeid(int32_t)) {
-	return reflection->GetInt32(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(int64_t)) {
-	return reflection->GetInt64(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(uint32_t)) {
-  	return reflection->GetUInt64(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(uint64_t)) {
-  	return reflection->GetUInt64(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(float)) {
-  	return reflection->GetFloat(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(double)) {
-  	return reflection->GetDouble(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(bool)) {
-  	return reflection->GetBool(*new_obj, rmes_field);
-  } else if (typeid(T) == typeid(string)) {
-  	return reflection->GetString(*new_obj, rmes_field);
-  } else {
-	// 如果什么类型都没匹配上，那么默认构造一个T，然后返回它
-	T t;
-	return t;
-  }*/
+uint32_t GetUInt32FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_UINT32) {
+    if (rmes_field->is_repeated()) {
+	  //判断是不是repeated字段
+	  return reflection->GetRepeatedUInt32(*new_obj, rmes_field, index);
+	} else {
+	  return reflection->GetUInt32(*new_obj, rmes_field);
+	}
+  }
+  return -1;
+}
+
+int64_t GetInt64FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_INT64) {
+    if (rmes_field->is_repeated()) {
+      //判断是不是repeated字段
+	  return reflection->GetRepeatedInt64(*new_obj, rmes_field, index);
+	} else {
+      return reflection->GetInt64(*new_obj, rmes_field);
+	}
+  }
+  return -1;
+}
+
+uint64_t GetUInt64FieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_UINT64) {
+    if (rmes_field->is_repeated()) {
+      //判断是不是repeated字段
+	  return reflection->GetRepeatedUInt64(*new_obj, rmes_field, index);
+	} else {
+      return reflection->GetUInt64(*new_obj, rmes_field);
+	}
+  }
+  return -1;
+}
+
+float GetFloatFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_FLOAT) {
+    if (rmes_field->is_repeated()) {
+	  //判断是不是repeated字段
+	  return reflection->GetRepeatedFloat(*new_obj, rmes_field, index);
+	} else {
+	  return reflection->GetFloat(*new_obj, rmes_field);
+    }
+  }
+  return -1.0;
+}
+
+double GetDoubleFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_DOUBLE) {
+    if (rmes_field->is_repeated()) {
+	  //判断是不是repeated字段
+	  return reflection->GetRepeatedDouble(*new_obj, rmes_field, index);
+	} else {
+	  return reflection->GetDouble(*new_obj, rmes_field);
+	}
+  }
+  return -1.0;
+}
+
+bool GetBoolFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_BOOL) {
+    if (rmes_field->is_repeated()) {
+      //判断是不是repeated字段
+	  return reflection->GetRepeatedBool(*new_obj, rmes_field, index);
+	} else {
+      return reflection->GetBool(*new_obj, rmes_field);
+	}
+  }
+  return false;
+}
+
+string GetStringFieldByPointer(boost::shared_ptr<::google::protobuf::Message> new_obj, const string& fieldName, int index) {
+
+  const ::google::protobuf::Reflection* reflection = new_obj->GetReflection();
+  const ::google::protobuf::FieldDescriptor* rmes_field = new_obj->GetDescriptor()->FindFieldByName(fieldName);
+  if (rmes_field->cpp_type() == ::google::protobuf::FieldDescriptor::CPPTYPE_STRING) {
+    if (rmes_field->is_repeated()) {
+	  //判断是不是repeated字段
+	  return reflection->GetRepeatedString(*new_obj, rmes_field, index);
+	 } else {
+	  	return reflection->GetString(*new_obj, rmes_field);
+	 }
+  }
+  return "Error";
 }
